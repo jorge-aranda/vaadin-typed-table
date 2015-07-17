4a4f5cb12b89f7e76d639455afb63621e25e545e
jorge-aranda <jorgearanda123@gmail.com>
2015-07-17 21:51:08 +0200

    First definition of TypedTable<T>

---
+++ vaadin-typed-table/src/main/es/jaranda/vaadinextensions/ui/TypedTable.java
@@ -0,0 +1,14 @@
+
+package es.jaranda.vaadinextensions.ui;
+
+/*
+ * Not tested
+ * Note: This implementation is EXPERIMENTAL until this comment will be
+ *       removed
+ */
+ 
+// TODO Complete JAVADOC for this class
+ 
+public class TypedTable<T> extends Table {
+    // TODO not implemented yet
+}
